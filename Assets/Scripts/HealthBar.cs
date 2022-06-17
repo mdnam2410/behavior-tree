9fa00537fb7eef6498913dbe86106ef58452a7c3
Nam Mai <mdnam2410@gmail.com>
2022-06-17 16:42:04 +0700

    Implement HealthBar

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -1,15 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class HealthBar : MonoBehaviour
 {
     [SerializeField]
     private Camera _mainCamera;
+    [SerializeField]
+    private TMP_Text _text;
+    [SerializeField]
+    private AttributeData _attributeData;
 
     void Update()
     {
         transform.LookAt(transform.position + _mainCamera.transform.rotation * Vector3.forward,
-                         _mainCamera.transform.rotation * Vector3.up);        
+                         _mainCamera.transform.rotation * Vector3.up);
+        _text.SetText($"Health: {_attributeData.HP}");
     }
 }
