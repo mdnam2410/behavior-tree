7f824c2a71d490ed947c0bb604e9e76bc62faa9d
Nam Mai <mdnam2410@gmail.com>
2022-06-17 17:18:00 +0700

    Implement health bar

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class HealthBar : MonoBehaviour
@@ -8,14 +9,19 @@ public class HealthBar : MonoBehaviour
     [SerializeField]
     private Camera _mainCamera;
     [SerializeField]
-    private TMP_Text _text;
-    [SerializeField]
     private AttributeData _attributeData;
+    [SerializeField]
+    private Slider _slider;
+
+    private void OnValidate()
+    {
+        _slider = GetComponent<Slider>();
+    }
 
     void Update()
     {
         transform.LookAt(transform.position + _mainCamera.transform.rotation * Vector3.forward,
                          _mainCamera.transform.rotation * Vector3.up);
-        _text.SetText($"Health: {_attributeData.HP}/{_attributeData.MaxHP}");
+        _slider.value = (float) (_attributeData.HP) / _attributeData.MaxHP;
     }
 }
